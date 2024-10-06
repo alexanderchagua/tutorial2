a32a3d8b2639e6800f919450eb6429645f4466eb
alexander <alexanderelton2887@gmail.com>
2024-10-05 23:49:35 -0500

    add new

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,45 @@
-export default function Page() {
-    return <p>customers page</p>;
+import CustomersTable from '@/app/ui/customers/table';
+import { Suspense } from 'react';
+import { Metadata } from 'next';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import { FormattedCustomersTable } from '@/app/lib/definitions';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+
+ 
+  const customers: FormattedCustomersTable[] = await fetchFilteredCustomers(query);
+
+ 
+  if (!customers || customers.length === 0) {
+    throw new Error('Failed to fetch customers');
   }
+
+  
+  const pageSize = 10;
+  const paginatedCustomers = customers.slice(
+    (currentPage - 1) * pageSize,
+    currentPage * pageSize
+  );
+
+  return (
+    
+    <Suspense fallback={<p>Loading...</p>}>
+      {paginatedCustomers.length > 0 && (
+        <CustomersTable customers={paginatedCustomers} />
+      )}
+    </Suspense>
+  );
+}
